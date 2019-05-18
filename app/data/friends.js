a102176a3dc596a7495e18b46f50f9d4721befd5
jax <jjackson17mv@gmail.com>
2019-05-18 01:13:03 -0700

    fix json package, server,friends, done

---
+++ app/data/friends.js
@@ -0,0 +1,42 @@
+var friends = [
+    {
+        friendName: "Pikachu",
+        friendimage: "https://www.indiewire.com/wp-content/uploads/2018/11/Screen-Shot-2018-11-12-at-12.05.31-PM.png?w=780",
+        scores: [
+            1,
+            1,
+            1,
+            3,
+            3,
+            4,
+            4,
+        ]
+    },
+    {
+        friendName: "Dead Pool",
+        friendimage: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Ferikkain%2Ffiles%2F2018%2F02%2Fdeadpool-2-teddy-bear.jpg",
+        scores: [
+            2,
+            2,
+            3,
+            1,
+            2,
+            3,
+        ]
+    },
+    
+    {
+        friendName: "Gavin Belson",
+        friendimage: "https://i.redd.it/ye63aue3gnt01.jpg",
+        scores: [
+           1,
+           2,
+           3,
+           4,
+           1,
+           2,
+        ]
+    },
+];
+
+module.exports = friends;
